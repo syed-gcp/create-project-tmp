9600c5d51a58977800c2b6b08d767bdffcd15e33
syed-gcp <63341417+syed-gcp@users.noreply.github.com>
2023-10-11 16:15:55 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+'use strict';
+
 const core = require('@actions/core');
-const { Resource } = require('@google-cloud/resource-manager');
+const { ProjectsClient } = require('@google-cloud/resource-manager').v3;
 
 async function run() {
   try {
@@ -7,17 +9,21 @@ async function run() {
     const projectName = core.getInput('project-name', { required: true });
     const projectId = core.getInput('project-id', { required: true });
 
-    // Initialize Google Cloud Resource Manager:
-    const resource = new Resource();
+    // Set up the ProjectsClient and request:
+    const resourcemanagerClient = new ProjectsClient();
+    const project = {
+      name: `projects/${projectId}`,
+      projectId: projectId,
+      displayName: projectName
+    };
 
-    // Create a new project:
-    const project = await resource.createProject({
-      name: projectName,
-      projectId: projectId
-    });
-
-    // Output the project ID:
-    core.setOutput('project-id', project.id);
+    // Run the request to create the project:
+    const [operation] = await resourcemanagerClient.createProject({ project });
+    const [response] = await operation.promise();
+    
+    // Log the response and set the output:
+    console.log(response);
+    core.setOutput('project-id', response.name); // You might want to output different information
 
   } catch (error) {
     // If there's an error, set the action as failed:
