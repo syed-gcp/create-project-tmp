3aec1d94147a99772a43086090f6ab8754c8e3bf
syed-goreply <135965588+syed-goreply@users.noreply.github.com>
2023-10-11 19:22:02 +0100

    add new files

--- index.js
+++ index.js
@@ -4,31 +4,31 @@ const core = require('@actions/core');
 const { ProjectsClient } = require('@google-cloud/resource-manager').v3;
 
 async function run() {
-  try {
-    // Get inputs from the workflow file:
-    const projectName = core.getInput('project-name', { required: true });
-    const projectId = core.getInput('project-id', { required: true });
+    try {
+        // Get inputs from the workflow file:
+        const projectName = core.getInput('project-name', { required: true });
+        const projectId = core.getInput('project-id', { required: true });
 
-    // Set up the ProjectsClient and request:
-    const resourcemanagerClient = new ProjectsClient();
-    const project = {
-      name: `projects/${projectId}`,
-      projectId: projectId,
-      displayName: projectName
-    };
+        // Set up the ProjectsClient and request:
+        const resourcemanagerClient = new ProjectsClient();
+        const project = {
+            name: `projects/${projectId}`,
+            projectId: projectId,
+            displayName: projectName
+        };
 
-    // Run the request to create the project:
-    const [operation] = await resourcemanagerClient.createProject({ project });
-    const [response] = await operation.promise();
-    
-    // Log the response and set the output:
-    console.log(response);
-    core.setOutput('project-id', response.name); // You might want to output different information
+        // Run the request to create the project:
+        const [operation] = await resourcemanagerClient.createProject({ project });
+        const [response] = await operation.promise();
 
-  } catch (error) {
-    // If there's an error, set the action as failed:
-    core.setFailed(error.message);
-  }
+        // Log the response and set the output:
+        console.log(response);
+        core.setOutput('project-id', response.name); // You might want to output different information
+
+    } catch (error) {
+        // If there's an error, set the action as failed:
+        core.setFailed(error.message);
+    }
 }
 
 // Run the action:
